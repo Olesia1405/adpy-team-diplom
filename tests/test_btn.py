997ace36a638f963153f21c0d7d69109f73c4e0a
Hors <iliya.engineer20@gmail.com>
2024-09-26 17:40:17 +0500

    Добавил новые проверки и тесты

--- tests/test_btn.py
+++ tests/test_btn.py
@@ -12,8 +12,10 @@ from vk_api.keyboard import VkKeyboardColor
 from btn_text import (
     BTN_REGISTRATION, BTN_FIND_PAIR, BTN_HELP, BTN_LIKE, BTN_DISLIKE,
     BTN_SEX_MAN, BTN_SEX_WOMAN, buttons_regist, buttons_start,
-    buttons_choice, buttons_choice_sex, WELCOME_MESSAGE, BTN_CHOSEN, BTN_MAIN_MENU
+    buttons_choice, buttons_choice_sex, WELCOME_MESSAGE, BTN_CHOSEN, BTN_MAIN_MENU, BTN_NEXT, BTN_BACK,
+    BTN_REMOVE_FAVORITES, buttons_favorites, buttons_favorites_next, buttons_favorites_back, HELP_MESSAGE
 )
+
 
 # Тестируем константы для названий кнопок
 def test_button_constants():
@@ -24,6 +26,12 @@ def test_button_constants():
     assert BTN_DISLIKE == "Не нравится👎"
     assert BTN_SEX_MAN == 'Кавалера🙎‍♂️'
     assert BTN_SEX_WOMAN == 'Женщину🙎‍♀️️'
+    assert BTN_MAIN_MENU == 'Главное меню🏠'
+    assert BTN_CHOSEN == "⭐Избранные⭐"
+    assert BTN_NEXT == 'Следующий🔜'
+    assert BTN_BACK == 'Предыдущий🔙'
+    assert BTN_REMOVE_FAVORITES == 'Убрать из избранных💔'
+
 
 # Тестируем списки кнопок и их цвета
 def test_buttons_regist():
@@ -32,12 +40,14 @@ def test_buttons_regist():
         (BTN_HELP, VkKeyboardColor.NEGATIVE)
     ]
 
+
 def test_buttons_start():
     assert buttons_start == [
         (BTN_FIND_PAIR, VkKeyboardColor.PRIMARY),
         (BTN_CHOSEN, VkKeyboardColor.POSITIVE),
         (BTN_HELP, VkKeyboardColor.NEGATIVE)
     ]
+
 
 def test_buttons_choice():
     assert buttons_choice == [
@@ -46,12 +56,36 @@ def test_buttons_choice():
         (BTN_MAIN_MENU, VkKeyboardColor.PRIMARY)
     ]
 
+
 def test_buttons_choice_sex():
     assert buttons_choice_sex == [
         (BTN_SEX_MAN, VkKeyboardColor.PRIMARY),
         (BTN_SEX_WOMAN, VkKeyboardColor.POSITIVE),
         (BTN_MAIN_MENU, VkKeyboardColor.PRIMARY)
     ]
+
+
+def test_buttons_favorites():
+    assert buttons_favorites == [(BTN_BACK, VkKeyboardColor.PRIMARY),
+                                 (BTN_NEXT, VkKeyboardColor.PRIMARY),
+                                 (BTN_REMOVE_FAVORITES, VkKeyboardColor.NEGATIVE),
+                                 (BTN_MAIN_MENU, VkKeyboardColor.POSITIVE)
+                                 ]
+
+
+def test_buttons_favorites_next():
+    assert buttons_favorites_next == [(BTN_NEXT, VkKeyboardColor.PRIMARY),
+                                      (BTN_REMOVE_FAVORITES, VkKeyboardColor.NEGATIVE),
+                                      (BTN_MAIN_MENU, VkKeyboardColor.POSITIVE)
+                                      ]
+
+
+def test_buttons_favorites_back():
+    assert buttons_favorites_back == [(BTN_BACK, VkKeyboardColor.PRIMARY),
+                                      (BTN_REMOVE_FAVORITES, VkKeyboardColor.NEGATIVE),
+                                      (BTN_MAIN_MENU, VkKeyboardColor.POSITIVE)
+                                      ]
+
 
 # Тестируем welcome_message
 def test_welcome_message():
@@ -63,3 +97,21 @@ def test_welcome_message():
         'Удачи в поиске, надеемся, что здесь ты найдёшь именно того, кого ищешь! 💫'
     )
     assert WELCOME_MESSAGE == expected_message
+
+
+def test_help_message():
+    expected_message = (
+                """
+                Этот бот поможет вам найти пару! 👫
+                
+                Команды бота:
+                - "Начать" — зарегистрируйтесь и начните поиск.
+                - "Найти пару" — выберите параметры для поиска кандидатов.
+                - "Помощь" — получите информацию о командах бота.
+                
+                Используйте кнопки на клавиатуре для быстрого доступа к функциям.
+                
+                Если возникли вопросы, обратитесь в поддержку. Удачи в поиске!
+                """
+    )
+    assert HELP_MESSAGE == expected_message
